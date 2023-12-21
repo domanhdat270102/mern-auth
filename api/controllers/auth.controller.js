e2098602ef6fa896396cbf7759efb1bed03f9f13
domanhdat270102 <107201023+domanhdat270102@users.noreply.github.com>
2023-12-22 05:54:19 +0700

    complete sign in

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -2,6 +2,7 @@ import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 import {errorHandler} from '../util/error.js'
 import {catchAsync} from '../util/catchAsync.js'
+import jwt from 'jsonwebtoken'
 
 export const signup = catchAsync(async (req, res, next) => {
     const {username, email, password} = req.body;
@@ -19,3 +20,14 @@ export const signup = catchAsync(async (req, res, next) => {
         message: "User created successfully"
     })
 });
+
+export const signin = catchAsync (async(req, res, next) => {
+    const {email, password} = req.body
+    const validUser = await User.findOne({email})
+    if (!validUser) return next(errorHandler(404, 'User not found'))
+    const validPassword = bcrypt.compareSync(password, validUser.password)
+    if (!validPassword) return next(errorHandler(401, 'Wrong credentials'))
+    const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET)
+    const {password: pass, ...rest} = validUser._doc
+    res.cookie('access_token', token, {httpOnly: true}).status(200).json(rest)
+})
