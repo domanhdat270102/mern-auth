9762e7ce21fad5a5d54e9979cc0e8dd9a06f32f4
domanhdat270102 <107201023+domanhdat270102@users.noreply.github.com>
2023-12-21 20:53:09 +0700

    middleware error

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,15 +1,21 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import {errorHandler} from '../util/error.js'
+import {catchAsync} from '../util/catchAsync.js'
 
-export const signup = async (req, res, next) => {
+export const signup = catchAsync(async (req, res, next) => {
     const {userName, email, password} = req.body;
     const newUser = new User({userName, email, password});
-    try {
-        await newUser.save()
-        res.status(200).json({
-            message: "User created successfully"
-        })
-    } catch (error) {
-        res.status(500).json(error.message)
-    }
-};
+    // try {
+    //     await newUser.save()
+    //     res.status(200).json({
+    //         message: "User created successfully"
+    //     })
+    // } catch (error) {
+    //     next(error);
+    // }
+    await newUser.save()
+    res.status(200).json({
+        message: "User created successfully"
+    })
+});
