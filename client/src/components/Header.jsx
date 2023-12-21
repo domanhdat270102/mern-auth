d0962dde252445ebbc7dcbdf0d6f7937941a0c34
domanhdat270102 <107201023+domanhdat270102@users.noreply.github.com>
2023-12-21 21:17:15 +0700

    reHeader

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,15 +1,40 @@
 import { Link } from "react-router-dom"
+import { FaSearch } from "react-icons/fa";
 export default function Header() {
   return (
-    <div className="bg-slate-200">
+    <header className="bg-slate-200 shadow-md">
         <div className="max-w-6xl mx-auto flex justify-between items-center p-3">
-            <p className="font-bold">Auth App</p>
+          <Link to='/'>
+            <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+              <span className="text-slate-500">Sahara</span>
+              <span className="text-slate-700">Estate</span>
+            </h1>
+          </Link>
+
+            <form className="bg-slate-100 p-3 rounded-lg flex items-center">
+              <input type="text" placeholder="Search..." className="bg-transparent focus:outline-none w-24 sm:w-64"/>
+              <FaSearch className="text-slate-600"/>
+            </form>
             <ul className="flex gap-4">
-                <Link to={'/'}>Home</Link>
-                <Link to={'/about'}>About</Link>
-                <Link to={'/signin'}>Sign in</Link>
+              <Link to={'/'}>
+                <li className="hidden sm:inline hover:underline text-slate-700">
+                  Home
+                </li>
+              </Link>
+
+              <Link to={'/about'}>
+                <li className="hidden sm:inline hover:underline text-slate-700">
+                  About
+                </li>
+              </Link>
+
+              <Link to={'/sign-in'}>
+                <li className="hidden sm:inline hover:underline text-slate-700">
+                  Sign in
+                </li>
+              </Link>
             </ul>
         </div>
-    </div>
+    </header>
   )
 }
