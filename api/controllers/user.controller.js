eb0a89a5c950a4fe7087af91f86cdd2c9e70935d
domanhdat270102 <107201023+domanhdat270102@users.noreply.github.com>
2023-12-22 22:24:41 +0700

    create api update user

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,5 +1,29 @@
+import User from '../models/user.model.js'
+import {catchAsync} from '../util/catchAsync.js'
+import { errorHandler } from '../util/error.js'
+import bcrypt from 'bcryptjs'
 export const test = (req,res) => {
     res.json({
         message: 'API is working'
     })
 }
+
+export const updateUser = catchAsync( async (req, res) => {
+    if (req.user.id !== req.params.id) return next(errorHandler(401, 'you can only update your own account!'));
+    if (req.body.password) {
+        req.body.password = bcrypt.hashSync(req.body.password, 10)
+    }
+    const updateUser = await User.findByIdAndUpdate(req.params.id, {
+        $set: {
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password,
+            avatar: req.body.avatar
+        }
+    }, {
+        new: true
+    })
+
+    const {password, ...rest} = updateUser._doc
+    res.status(200).json(rest)
+})
