b2e86ee9088c3b6f4308b33cd3f6e1de52bef602
domanhdat270102 <107201023+domanhdat270102@users.noreply.github.com>
2023-12-23 09:40:36 +0700

    update user update functional

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,16 +1,18 @@
 import { useRef, useState, useEffect } from "react"
-import { useSelector } from "react-redux"
+import { useSelector, useDispatch } from "react-redux"
 import {getDownloadURL, getStorage, ref, uploadBytesResumable} from 'firebase/storage'
 import { app } from "../firebase"
+import {updateUserFailure, updateUserSuccess, updateUserStart} from '../redux/user/userSlice'
 
 export default function Profile() {
-  const {currentUser} = useSelector(state => state.user)
+  const {currentUser, loading, error} = useSelector(state => state.user)
+  const dispatch = useDispatch()
   const fileRef = useRef(null)
   const [file, setFile] = useState(undefined)
   const [filePerc, setFilePerc] = useState(0)
   const [fileUploadError, setFileUploadError] = useState(false)
+  const [updateSuccess, setUpdateSuccess] = useState(false)
   const [formData, setFormData] = useState({})
-
   console.log(formData);
   // allow read;
   // allow write: if
@@ -42,13 +44,41 @@ export default function Profile() {
       getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => setFormData({ ...formData, avatar: downloadURL})) 
   });
 };
+
+const handleChange = (e) => {
+  setFormData({...formData, [e.target.id]: e.target.value})
+}
+
+const handleSubmit = async (e) => {
+  e.preventDefault()
+  try {
+    dispatch(updateUserStart())
+    const res = await fetch(`/api/user/update/${currentUser._id}`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json'},
+      body: JSON.stringify(formData)
+    })
+
+    const data = await res.json()
+
+    if (data.success === false) {
+      dispatch(updateUserFailure(data.message))
+      return;
+    }
+
+    dispatch(updateUserSuccess(data))
+    setUpdateSuccess(true)
+  } catch (error) {
+    dispatch(updateUserFailure(error.message))
+  }
+}
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">
           Profile
       </h1>
 
-      <form className="flex flex-col gap-4">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
           <input
             onChange={(e) => setFile(e.target.files[0])} 
             type="file" ref={fileRef} 
@@ -56,7 +86,7 @@ export default function Profile() {
             accept="image/*"/> 
           <img 
             onClick={() => fileRef.current.click()} 
-            src={formData.avatar || currentUser.avatar} 
+            src={formData?.avatar || currentUser.avatar} 
             alt="profile" 
             className="rounded-full w-24 h-24 self-center object-cover cursor-pointer mt-2"
           />
@@ -76,11 +106,31 @@ export default function Profile() {
             ): ""}
             </p>
 
-          <input type="text" placeholder="username" id="username" className="border p-3 rounded-lg focus:outline-none"/>
-          <input type="email" placeholder="email" id="email" className="border p-3 rounded-lg focus:outline-none"/>
-          <input type="password" placeholder="password" id="password" className="border p-3 rounded-lg focus:outline-none"/>
-          <button className="p-3 bg-slate-700 text-white uppercase rounded-lg hover:opacity-95 disabled:opacity-80">
-              Update
+          <input 
+            type="text" 
+            placeholder="username" 
+            id="username" 
+            className="border p-3 rounded-lg focus:outline-none"
+            defaultValue={currentUser.username}
+            onChange={handleChange}
+          />
+          <input 
+            type="email" 
+            placeholder="email" 
+            id="email" 
+            className="border p-3 rounded-lg focus:outline-none"
+            defaultValue={currentUser.email}
+            onChange={handleChange}
+          />
+          <input 
+            type="password" 
+            placeholder="password" 
+            id="password" 
+            className="border p-3 rounded-lg focus:outline-none"
+            onChange={handleChange}
+          />
+          <button disabled={loading} className="p-3 bg-slate-700 text-white uppercase rounded-lg hover:opacity-95 disabled:opacity-80">
+              {loading ? "Loading...": "Update"}
           </button>
 
           <div className="flex justify-between">
@@ -89,6 +139,14 @@ export default function Profile() {
             </span>
             <span className="text-red-700 cursor-pointer">Sign out</span>
           </div>
+
+          <p className='text-red-700 mt-5'>
+              {error ? error :  ""}
+          </p>
+
+          <p className='text-green-700 mt-5'>
+              {updateSuccess ? "User is updated successfully!" :  ""}
+          </p>
       </form>
     </div>
   )
