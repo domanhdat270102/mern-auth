e1056144de131f86b47803d46e71b588ca3afa0d
domanhdat270102 <107201023+domanhdat270102@users.noreply.github.com>
2023-12-24 09:41:42 +0700

    create update listing api

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -21,3 +21,20 @@ export const deleteListing = catchAsync(async (req, res, next) => {
     await Listing.findByIdAndDelete(req.params.id)
     res.status(200).json('Listing has been deleted!')
 })
+
+export const updateListing = catchAsync(async (req, res, next) => {
+    const listing = await Listing.findById(req.params.id)
+
+    if (!listing) {
+        return next(errorHandler(404, 'Listing not found'));
+    }
+
+    if (req.user.id !== listing.useRef) {
+        return next(errorHandler(401, 'You can delete your own listings'))
+    }
+
+    const updatedListing = await Listing.findByIdAndUpdate(req.params.id, req.body, {
+        new: true
+    })
+    res.status(200).json(updatedListing)
+})
--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { creatListing, deleteListing } from '../controllers/listing.controller.js';
+import { creatListing, deleteListing, updateListing } from '../controllers/listing.controller.js';
 import { verifyToken } from '../util/verifyUser.js';
 
 const router = express.Router()
 
 router.post('/create', verifyToken, creatListing)
 router.delete('/delete/:id', verifyToken, deleteListing)
+router.post('/update/:id', verifyToken, updateListing)
 
 export default router
